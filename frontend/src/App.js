34c7e4a2fbab1775c0e2781f7088f36c7fff6c43
Oscar Umana <oscarumana1021@gmail.com>
2023-04-10 00:06:56 -0400

    theme changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,37 @@
 import NavBar from "./components/NavBar";
 import { ModelProvider } from "./context/ModelContext";
 import Form from "./pages/Form";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#865DFF',
+    },
+    mode: "dark",
+    background: {
+    },
+    text: {
+      primary: "#865DFF",
+      secondary: "#E384FF",
+    },
+  },
+});
 
 function App() {
   return (
-    <ModelProvider>
-      <div
-        className="App"
-        style={{ height: "100vh", maxWidth: "75%", margin: "0 auto" }}
-      >
-        <NavBar />
-        <Form />
-      </div>
-    </ModelProvider>
+    <ThemeProvider theme={darkTheme}>
+      <ModelProvider>
+        <div
+          className="App"
+          style={{ height: "100vh", maxWidth: "75%", margin: "0 auto" }}
+        >
+          <NavBar />
+          <Form />
+        </div>
+      </ModelProvider>
+    </ThemeProvider>
   );
 }
 
